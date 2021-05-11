a1128c3e39943e5689104f510d20ac182d9c96b0
Shreyas Vissapragada <svissapr@caltech.edu>
2021-05-11 12:30:21 -0700

    switching to setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ import setuptools
 
 setuptools.setup(
 	name = "exowirc",
-	url = "https://github.com/astroshrey/exowirc",
 	version = '0.0.1',
+	url = "https://github.com/astroshrey/exowirc",
 	author = "Shreyas Vissapragada",
 	author_email = "svissapr@caltech.edu",
+	packages = setuptools.find_packages(),
 	packages = ["exowirc"],
 	license = "MIT",
 	install_requires=["numpy", "scipy", "astropy", "matplotlib", 
-		"cv2", "exoplanet", "theano", "lightkurve", "pymc3",
-		"arviz", "photutils", "corner"]
+		"opencv-python", "exoplanet", "lightkurve", "pymc3",
+		"arviz", "photutils", "corner", "setuptools"]
 	)
