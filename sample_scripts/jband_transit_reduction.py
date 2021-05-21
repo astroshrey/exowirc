753b0303e019bfe32a9ea0af825a7a7f1c6ef203
Shreyas Vissapragada <svissapr@caltech.edu>
2021-05-21 15:14:23 -0500

    finalized jband redux

--- sample_scripts/jband_transit_reduction.py
+++ sample_scripts/jband_transit_reduction.py
@@ -6,10 +6,9 @@ import numpy as np
 import warnings
 
 ######## pipeline steps #################################
-remake_darks_and_flats = True 
-remake_bkg = True
-calibrate_data = True
-photometric_extraction = True
+remake_darks_and_flats = False 
+calibrate_data = False
+photometric_extraction = False 
 fit_for_eclipse = True
 ######## calibration params ###############
 data_dir = '/Volumes/brassnose/20190824/'
@@ -21,11 +20,8 @@ science_seqs = [(65, 456)]
 dark_seqs = [(458, 477)]
 flat_seq = (22, 41)
 dark_for_flat_seq = (2, 21)
-bkg_seq = (60, 63)
-bkg_sigma_lower = 5
-bkg_sigma_upper = 1000
 background_mode = 'median'
-covariate_names = ['d_from_med', 'background', 'airmass']
+covariate_names = []
 ####### extraction params ###############
 source_coords = [1210, 671]
 finding_fwhm = 15.
@@ -38,10 +34,10 @@ phase = 'primary'
 texp = (50./60.)/1440. #days
 r_star_prior = ('normal', 1.01, 0.045) #Berger+18
 period_prior = ('normal', 125.8518, 0.0076) #Schmitt+14
-t0_prior = ('normal', 2455069.8518, 0.0077) #Schmitt+14
+t0_prior = ('uniform', 2458719.4, 2458720.)
 a_rs_prior = ('normal', 108.6, 1.1) #Schmitt+14
 b_prior = ('normal', 0.394, 0.029) #Schmitt+14
-ror_prior = ('uniform', 0., 0.25)
+ror_prior = ('uniform', 0., 0.15)
 jitter_prior = ('uniform', 1e-6, 1e-2)
 ######## fitting params ###############
 tune = 1000            #number of burn-in steps per chain
@@ -53,25 +49,14 @@ if __name__ == '__main__':
 	calib_dir, dump_dir, img_dir = iu.init_output_direcs(output_dir,
 		test_name)	
 
-	with warnings.catch_warnings():
-		warnings.simplefilter("ignore")
-		bkg = cu.make_calibrated_bkg_image(data_dir, calib_dir,	bkg_seq,
-			dark_seqs, dark_for_flat_seq, flat_seq,
-			naming_style = naming_style, 
-			nonlinearity_fname = nonlinearity_fname,
-			sigma_lower = bkg_sigma_lower, 
-			sigma_upper = bkg_sigma_upper, 
-			remake_darks_and_flats = remake_darks_and_flats,
-			remake_bkg = remake_bkg)
-
 	if calibrate_data:
 		with warnings.catch_warnings():
 			warnings.simplefilter("ignore")
 			cu.calibrate_all(data_dir, calib_dir, dump_dir,
 				science_seqs, dark_seqs, dark_for_flat_seq,
 				flat_seq, style = naming_style, 
-				background_mode = background_mode, 
-				bkg_filename = bkg)
+				background_mode = background_mode,
+				remake_darks_and_flats = remake_darks_and_flats)
 
 	if photometric_extraction:
 		with warnings.catch_warnings():
@@ -84,8 +69,7 @@ if __name__ == '__main__':
 				background_mode = background_mode,
 				ann_rads = ann_rads,
 				source_detection_sigma = source_detection_sigma,
-				max_num_compars = max_num_compars,
-				bkg_fname = bkg)
+				max_num_compars = max_num_compars)	
 
 	if fit_for_eclipse:
 		with warnings.catch_warnings():
